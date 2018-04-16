9c42bd0a17745f87a66b163141c53825d0202f15
smjung <smjung@ahnlab.com>
2018-04-16 13:42:33 +0900

    delete all

--- RemoteServer/PreBatch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-title Pre Batch File
-
-rem write the command below this line
-rem ----------------------------------------------------------------------------------------
-start cmd /C C:\\lib\\RunRemote_SeleniumLibrary.bat %1
-rem ----------------------------------------------------------------------------------------
