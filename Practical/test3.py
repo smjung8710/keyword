f0bf8b10be4ecc3c6123457522cad8d878d3aadc
smjung8710 <smjung8710@gmail.com>
2018-07-19 17:30:26 +0900

    delete practical folder

--- Practical/test3.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-machine=platform.machine()
-if machine == "AMD64":
-    machine = 'x64'
-else:
-    machine = 'x86'
-print(machine)
--- Practical/test8.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: cp949 -*-
-from pywinauto.application import Application
-
-app = Application().start("notepad.exe")
-app.menu_select("도움말->메모장정보")
-app.AboutNotepad.OK.click()
