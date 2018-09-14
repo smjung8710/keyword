443d6d6f0b35b9a1b0445c8690101d851c4e11a6
smjung8710 <smjung8710@gmail.com>
2018-09-14 18:02:09 +0900

    Update command.py

--- TestLib/linux_remote/command.py
+++ TestLib/linux_remote/command.py
@@ -1,5 +1,3 @@
 import os, system
 
 os.system(robot -i count -v IP C:\Keyword\TestCase\3_4_RemoteKeyword.robotframework)
-os.system(robot -i count -v IP C:\Keyword\TestCase\3_4_RemoteKeyword.robotframework)
-os.system(robot -i count -v IP C:\Keyword\TestCase\3_4_RemoteKeyword.robotframework)
